4eb06f9e5d8e10bf03f9ca080ed27b9da9803cb0
Bedivierre <bookvariant@list.ru>
2020-08-04 18:00:59 +0300

    Исправление для работы с обновленной версией Craftsman

--- src/Responses/BaseResponse.php
+++ src/Responses/BaseResponse.php
@@ -5,18 +5,24 @@ namespace Bedivierre\Sberbank\Responses;
 
 
 use Bedivierre\Craftsman\Aqueduct\BaseResponseObject;
+use Bedivierre\Craftsman\Masonry\BaseDataObject;
 
 class BaseResponse extends \Bedivierre\Craftsman\Aqueduct\BaseResponseObject
 {
     public function __construct(BaseResponseObject $response)
     {
-        parent::__construct([], $response->getUrl(), $response->getMethod());
+        $data = new BaseDataObject([
+            'headers' => $response->getAllHeaders(),
+            'code'=> $response->getHttpCode(),
+            'body'=> $response,
+        ]);
+        parent::__construct($data, $response->getUrl(), $response->getMethod());
 
         if($response->hasError()){
             $this->setError($response->errorMessage(), $response->errorCode());
             return;
         }
 
-        $this->absorb($response);
+        $this->absorb($response, true, true);
     }
 }
